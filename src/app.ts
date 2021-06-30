d5e013de66a8496e1a2e3159f746e8aa07d271d6
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-06-30 14:47:33 +0200

    Update route name

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/delivery', deliveryRouter);
+app.use('/deliveries', deliveryRouter);
 
 module.exports = app;
